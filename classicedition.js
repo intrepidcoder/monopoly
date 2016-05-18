b875e26202797d7f2e7a17c8a5b1a6f4d4ea3970
paigeblue08 <paigeblue08@gmail.com>
2016-05-17 21:03:49 -0500

    Just testing
    For a school project.

--- classicedition.js
+++ classicedition.js
@@ -55,61 +55,61 @@ function transtext() {
 }
 
 function luxurytax() {
-	addAlert(player[turn].name + " paid $100 for landing on Luxury Tax.");
+	addAlert(player[turn].name + " paid $100 for landing on Floods.");
 	player[turn].pay(100, 0);
 	
-	$("#landed").show().text("You landed on Luxury Tax. Pay $100.");
+	$("#landed").show().text("You landed on Floods. Pay $100.");
 }
 
 function citytax() {
-	addAlert(player[turn].name + " paid $200 for landing on City Tax.");
+	addAlert(player[turn].name + " paid $200 for landing on Earthquakes.");
 	player[turn].pay(200, 0);
 
-	$("#landed").show().text("You landed on City Tax. Pay $200.");
+	$("#landed").show().text("You landed on Earthquakes. Pay $200.");
 }
 
 var square = [];
 
-square[0] = new Square("GO", "COLLECT $200 SALARY AS YOU PASS.", "#FFFFFF");
-square[1] = new Square("Mediterranean Avenue", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250);
-square[2] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[3] = new Square("Baltic Avenue", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450);
-square[4] = new Square("City Tax", "Pay $200", "#FFFFFF");
-square[5] = new Square("Reading Railroad", "$200", "#FFFFFF", 200, 1);
-square[6] = new Square("Oriental Avenue", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
+square[0] = new Square("GO", "COLLECT $200 AS YOU PASS.", "#FFFFFF");
+square[1] = new Square("Suriname", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250);
+square[2] = new Square("Fun Facts", "FOLLOW INSTRUCTIONS ON TOP CARD AND THEN MOVE TO THAT COUNTRY", "#FFFFFF");
+square[3] = new Square("Uruguay", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450);
+square[4] = new Square("Earthquakes", "Pay $200", "#FFFFFF");
+square[5] = new Square("Learn Dutch", "$200", "#FFFFFF", 200, 1);
+square[6] = new Square("Guyana", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
 square[7] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[8] = new Square("Vermont Avenue", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
-square[9] = new Square("Connecticut Avenue", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600);
+square[8] = new Square("Ecuador", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
+square[9] = new Square("Paraguay", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600);
 square[10] = new Square("Just Visiting", "", "#FFFFFF");
-square[11] = new Square("St. Charles Place", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
-square[12] = new Square("Electric Company", "$150", "#FFFFFF", 150, 2);
-square[13] = new Square("States Avenue", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
-square[14] = new Square("Virginia Avenue", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900);
-square[15] = new Square("Pennsylvania Railroad", "$200", "#FFFFFF", 200, 1);
-square[16] = new Square("St. James Place", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
-square[17] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[18] = new Square("Tennessee Avenue", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
-square[19] = new Square("New York Avenue", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000);
-square[20] = new Square("Free Parking", "", "#FFFFFF");
-square[21] = new Square("Kentucky Avenue", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[11] = new Square("Chile", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
+square[12] = new Square("Manufactured Products", "$150", "#FFFFFF", 150, 2);
+square[13] = new Square("Venezuela", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
+square[14] = new Square("Bolivia", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900);
+square[15] = new Square("Learn French", "$200", "#FFFFFF", 200, 1);
+square[16] = new Square("Colombia", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
+square[17] = new Square("Fun Facts", "FOLLOW INSTRUCTIONS ON TOP CARD AND THEN MOVE TO THAT COUNTRY", "#FFFFFF");
+square[18] = new Square("Peru", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
+square[19] = new Square("Argentina", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000);
+square[20] = new Square("Earn money from relief organizations", "", "#FFFFFF");
+square[21] = new Square("Brazil", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
 square[22] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[23] = new Square("Indiana Avenue", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
-square[24] = new Square("Illinois Avenue", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100);
-square[25] = new Square("B&O Railroad", "$200", "#FFFFFF", 200, 1);
-square[26] = new Square("Atlantic Avenue", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
-square[27] = new Square("Ventnor Avenue", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
-square[28] = new Square("Water Works", "$150", "#FFFFFF", 150, 2);
-square[29] = new Square("Marvin Gardens", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200);
+square[23] = new Square("French Guiana", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[24] = new Square("Falkland Islands", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100);
+square[25] = new Square("Learn Portuguese", "$200", "#FFFFFF", 200, 1);
+square[26] = new Square("South Georgia and South Sandwich Islands", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[27] = new Square("Galapagos Islands", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[28] = new Square("Agricultural Products", "$150", "#FFFFFF", 150, 2);
+square[29] = new Square("Andes Mountains", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200);
 square[30] = new Square("Go to Jail", "Go directly to Jail. Do not pass GO. Do not collect $200.", "#FFFFFF");
-square[31] = new Square("Pacific Avenue", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275);
-square[32] = new Square("North Carolina Avenue", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275);
-square[33] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[34] = new Square("Pennsylvania Avenue", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400);
-square[35] = new Square("Short Line", "$200", "#FFFFFF", 200, 1);
+square[31] = new Square("Amazon River", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275);
+square[32] = new Square("Amazon Rainforest", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275);
+square[33] = new Square("Fun Facts", "FOLLOW INSTRUCTIONS ON TOP CARD AND THEN MOVE TO THAT COUNTRY", "#FFFFFF");
+square[34] = new Square("Amazon Basin", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400);
+square[35] = new Square("Learn Spanish", "$200", "#FFFFFF", 200, 1);
 square[36] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[37] = new Square("Park Place", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500);
-square[38] = new Square("LUXURY TAX", "Pay $100", "#FFFFFF");
-square[39] = new Square("Boardwalk", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000);
+square[37] = new Square("Atlantic Ocean", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500);
+square[38] = new Square("Floods", "Pay $100", "#FFFFFF");
+square[39] = new Square("Pacific Ocean", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000);
 
 var communityChestCards = [];
 var chanceCards = [];
